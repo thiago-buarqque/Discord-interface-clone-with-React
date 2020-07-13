a9738d83c6de45fc6c6e3c26736f73c2cfe2ebf2
Thiago Buarque <57292581+thiago-buarqque@users.noreply.github.com>
2020-07-12 22:37:30 -0300

    Update index.jsx

--- src/components/ServerInfo/index.jsx
+++ src/components/ServerInfo/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {Container, ServerName, OpenServerInfo, ServerNameContainer} from './styles.js'
 
 const ServerInfo = (props) =>{
-    console.log(`Isso foi o que chegou`,props.changeBackgroundServerInfo);
     return (
         <Container changeBackgroundServerInfo={props.changeBackgroundServerInfo}>            
             <ServerNameContainer changeBackgroundServerInfo={props.changeBackgroundServerInfo}>
